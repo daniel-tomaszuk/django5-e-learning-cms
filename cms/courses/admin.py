d438e2a478ceb0291f544f16c559980a5ac90b2b
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-06-03 16:31:04 +0200

    Add courses models and fixtures

--- cms/courses/admin.py
+++ cms/courses/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course
+from courses.models import Module
+from courses.models import Subject
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = [Subject.Keys.title, Subject.Keys.slug]
+    prepopulated_fields = {Subject.Keys.slug: (Subject.Keys.title,)}
+
+
+class ModuleInLine(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [Course.Keys.title, Course.Keys.subject, Course.Keys.created]
+    list_filter = [Course.Keys.created, Course.Keys.subject]
+    search_fields = [Course.Keys.title, Course.Keys.overview]
+    prepopulated_fields = {Course.Keys.slug: (Course.Keys.title,)}
+    inlines = [ModuleInLine]
--- cms/courses/models.py
+++ cms/courses/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Subject(models.Model):
+
+    class Keys:
+        id = "id"
+        title = "title"
+        slug = "slug"
+
+        # relations
+        courses = "courses"
+
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
+class Course(models.Model):
+
+    class Keys:
+        id = "id"
+        title = "title"
+        slug = "slug"
+        overview = "overview"
+        created = "created"
+        updated = "updated"
+
+        # relations
+        owner = "owner"
+        subject = "subject"
+        modules = "modules"
+
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    owner = models.ForeignKey(
+        User, related_name="courses_created", on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(
+        Subject, related_name="courses", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
+class Module(models.Model):
+
+    class Keys:
+        id = "id"
+        title = "title"
+        description = "description"
+        course = "course"
+
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+
+    course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.title)
