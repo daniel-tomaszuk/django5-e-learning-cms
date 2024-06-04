f12badcac214b9229e110bbbd637f3c579c0fb11
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-06-04 09:54:10 +0200

    Add course module and module item models.

--- cms/courses/models.py
+++ cms/courses/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 User = get_user_model()
@@ -65,7 +67,10 @@ class Module(models.Model):
         id = "id"
         title = "title"
         description = "description"
+
+        # relations
         course = "course"
+        contents = "contents"
 
     title = models.CharField(max_length=256)
     description = models.TextField(blank=True)
@@ -74,3 +79,108 @@ class Module(models.Model):
 
     def __str__(self) -> str:
         return str(self.title)
+
+
+class Content(models.Model):
+    class Keys:
+        id = "id"
+        module = "module"
+        content_type = "content_type"
+        object_id = "object_id"
+        item = "item"
+
+    module = models.ForeignKey(
+        Module, related_name="contents", on_delete=models.CASCADE
+    )
+
+    # generic key used to point at different models, only few are allowed
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            "model__in": ("text", "video", "image", "file"),
+        },
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+
+class ItemBase(models.Model):
+    class Keys:
+        id = "id"
+        title = "title"
+        created = "created"
+        updated = "updated"
+
+        owner = "owner"
+
+    title = models.CharField(max_length=256)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    owner = models.ForeignKey(
+        User, related_name="%(class)s_related", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
+class Text(ItemBase):
+    class Keys:
+        id = "id"
+        title = "title"
+        created = "created"
+        updated = "updated"
+        owner = "owner"
+
+        content = "content"
+
+    content = models.TextField()
+
+
+class File(ItemBase):
+    UPLOAD_DIR = "files"
+
+    class Keys:
+        id = "id"
+        title = "title"
+        created = "created"
+        updated = "updated"
+        owner = "owner"
+
+        file = "file"
+
+    file = models.FileField(upload_to=UPLOAD_DIR)
+
+
+class Image(ItemBase):
+    UPLOAD_DIR = "images"
+
+    class Keys:
+        id = "id"
+        title = "title"
+        created = "created"
+        updated = "updated"
+        owner = "owner"
+
+        file = "file"
+
+    file = models.FileField(upload_to=UPLOAD_DIR)
+
+
+class Video(ItemBase):
+
+    class Keys:
+        id = "id"
+        title = "title"
+        created = "created"
+        updated = "updated"
+        owner = "owner"
+
+        url = "url"
+
+    url = models.URLField()
