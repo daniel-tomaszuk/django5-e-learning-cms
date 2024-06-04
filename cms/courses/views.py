3314a30717f07970f25ae9c9c93475a6ede460b3
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-06-04 12:34:51 +0200

    Add course CRUD views.

--- cms/courses/views.py
+++ cms/courses/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import ListView
+from django.views.generic import UpdateView
 
-# Create your views here.
+from courses.models import Course
+
+
+class OwnerMixin:
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        return queryset.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form) -> bool:
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = [
+        Course.Keys.subject,
+        Course.Keys.title,
+        Course.Keys.slug,
+        Course.Keys.overview,
+    ]
+    success_url = reverse_lazy("manage_course_list")
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = "courses/manage/course/form.html"
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = "courses/manage/course/list.html"
+
+    def get_queryset(self) -> QuerySet[Course]:
+        queryset = super().get_queryset()
+        return queryset.filter(owner=self.request.user)
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = "courses/manage/course/delete.html"
