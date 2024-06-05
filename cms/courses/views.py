9547bf24cd7b01e81f35c5f57943407c8f0a6181
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-06-05 12:35:17 +0200

    Add modules drag and drop feature.

--- cms/courses/views.py
+++ cms/courses/views.py
@@ -1,3 +1,5 @@
+from braces.views import CsrfExemptMixin
+from braces.views import JsonRequestResponseMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.db import models
@@ -189,3 +191,27 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id: int):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response(context=dict(module=module))
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    """
+    Used for drag-and-drop feature. Updates order of mudules.
+    """
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response(context_dict=dict(saved="OK"))
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    """
+    Used for drag-and-drop feature. Updates order of module contents.
+    """
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, course__owner=request.user).update(
+                order=order
+            )
+        return self.render_json_response(context_dict=dict(saved="OK"))
